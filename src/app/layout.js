f4de4aa4c2c4da06e824d8643540167e8b690595
rishabh-star <risingmerta@gmail.com>
2025-09-05 19:31:00 +0530

    first commit

--- src/app/layout.js
+++ src/app/layout.js
@@ -37,16 +37,9 @@ export default function RootLayout({ children }) {
           `,
           }}
         />
-        {/* container required by the ad script */}
-        <div id="container-917495758f35e40004f3af3fe8c04eb8"></div>
-
-        {/* load external ad script via Next.js Script component */}
         <Script
-          id="adsterra-917495758f35e40004f3af3fe8c04eb8"
-          src="//decencytopmost.com/917495758f35e40004f3af3fe8c04eb8/invoke.js"
+          src="//decencytopmost.com/ea/1c/79/ea1c798183baca79e1812cf43c17e87e.js"
           strategy="afterInteractive"
-          data-cfasync="false"
-          async={true}
         />
       </head>
       <body className={inter.className}>
